10b98fe313e63963ef751adc22bbe03c04c6793d
Mahesh <mahesh4521@gmail.com>
2023-06-21 21:50:48 +0530

    1st commit

--- src/main/java/com/in36minutes/learningspringframework/HelloWorld.java
+++ src/main/java/com/in36minutes/learningspringframework/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello.......");
 		System.out.println("hi---> java pro");
-
+		System.out.println("hello.......c");
+		System.out.println("hi---> springBoot");
 	}
 
 }
